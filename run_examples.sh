719ad79949103d223d8c58f7aa792249d871f4d9
Aiden Nibali <dismaldenizen@gmail.com>
2017-11-20 15:57:18 +1100

    Use .md extension for examples

--- run_examples.sh
+++ run_examples.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Running the examples to produce HTML reports..."
-find examples -name "*.pmd" | xargs -n 1 pweave -f md2html
+find examples -name "*.md" | xargs -n 1 pweave -f md2html
