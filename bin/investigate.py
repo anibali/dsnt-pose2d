5a4476c487a26eb775d25b577bb68a195d78b490
Aiden Nibali <dismaldenizen@gmail.com>
2018-10-09 15:51:36 +1100

    Mistake vector field

--- bin/investigate.py
+++ bin/investigate.py
@@ -4,12 +4,19 @@
 This script is for investigating misclassified examples.
 """
 
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from seaborn import cubehelix_palette
+
 import argparse
 import random
 
-import torch
 import h5py
 import numpy as np
+import torch
+from scipy.stats import binned_statistic_2d
+from scipy.signal import convolve
 
 from dsnt.data import MPIIDataset
 
@@ -43,7 +50,7 @@ def seed_random_number_generators(seed):
 
 
 def main():
-    """Main evaluation entrypoint function."""
+    """Main investigation entrypoint function."""
 
     args = parse_args()
     seed_random_number_generators(args.seed)
@@ -67,23 +74,81 @@ def main():
         head_lengths = torch.from_numpy(f['normalize'][:])
     joint_mask = actual.select(2, 0).gt(1).mul(actual.select(2, 1).gt(1))
 
-    total_misclassified = 0
-    edge_misclassified = 0
+    true_xs = []
+    true_ys = []
+    pred_xs = []
+    pred_ys = []
+
+    txs = []
+    tys = []
 
     for i in range(len(dataset)):
         sample = dataset[i]
         norm_targets = sample['part_coords']
+        norm_preds = torch.mm((preds[i] - sample['transform_b']).squeeze(), torch.inverse(sample['transform_m']))
 
         for j in range(n_joints):
-            if joint_mask[i, j] == 1:
+            if joint_mask[i, j] == 1 and np.abs(norm_targets[j, 0]) <= 1 and np.abs(norm_targets[j, 1]) <= 1:
+                txs.append(norm_targets[j, 0])
+                tys.append(norm_targets[j, 1])
                 dist = torch.dist(actual[i, j], preds[i, j]) / head_lengths[i]
                 if dist > threshold:
-                    total_misclassified += 1
-                    if norm_targets[j].abs().max() > 0.9:
-                        edge_misclassified += 1
+                    true_xs.append(norm_targets[j, 0])
+                    true_ys.append(norm_targets[j, 1])
+                    pred_xs.append(norm_preds[j, 0])
+                    pred_ys.append(norm_preds[j, 1])
 
-    print('total_misclassified', total_misclassified)
-    print('edge_misclassified: {} ({:.2f}%)'.format(edge_misclassified, 100.0 * edge_misclassified / total_misclassified))
+    true_xs = np.array(true_xs)
+    true_ys = np.array(true_ys)
+    pred_xs = np.array(pred_xs)
+    pred_ys = np.array(pred_ys)
+
+    xstats = binned_statistic_2d(true_xs, true_ys, pred_xs - true_xs, statistic='mean', bins=8,
+                                       range=[[-1, 1], [-1, 1]])
+    ystats = binned_statistic_2d(true_xs, true_ys, pred_ys - true_ys, statistic='mean', bins=8,
+                                       range=[[-1, 1], [-1, 1]])
+
+    vector_field = np.stack((np.array(xstats.statistic).transpose(), np.array(ystats.statistic).transpose()), axis=-1)
+
+    counts = np.array(binned_statistic_2d(true_xs, true_ys, None, statistic='count', bins=8,
+                                       range=[[-1, 1], [-1, 1]]).statistic).transpose()
+    totals = np.array(binned_statistic_2d(txs, tys, None, statistic='count', bins=8,
+                                       range=[[-1, 1], [-1, 1]]).statistic).transpose()
+    with np.errstate(divide='ignore', invalid='ignore'):
+        C = np.nan_to_num(counts / totals)
+
+    norm = matplotlib.colors.Normalize(vmin=np.min(C), vmax=np.max(C))
+    cmap = cubehelix_palette(8, start=0.0, rot=0.85, as_cmap=True)
+    smap = matplotlib.cm.ScalarMappable(cmap=cmap, norm=norm)
+    smap.set_array([])
+
+    # This quiver plot shows the average error offset for misclassified joints, binned by
+    # grid cell. So the arrows show where the predictions tend to lie relative to the ground
+    # truth location. The colour of the arrows indicates the % of misclassified joints for that
+    # cell.
+    fig = plt.figure(figsize=(6, 6))
+    ax = fig.add_subplot(111, aspect='equal')
+    indices = np.unravel_index(np.argsort(C.flatten()), C.shape)
+    X = convolve(xstats.x_edge, np.array([0.5, 0.5]), mode='valid')
+    Y = convolve(xstats.y_edge, np.array([0.5, 0.5]), mode='valid')
+    X, Y = np.meshgrid(X, Y)
+    X = X[indices]
+    Y = Y[indices]
+    U = vector_field[..., 0][indices]
+    V = vector_field[..., 1][indices]
+    C = C[indices]
+    ax.quiver(X, Y, U, V, C, cmap=cmap, angles='xy', scale_units='xy', scale=1, zorder=2)
+    ax.set_xlim(-1.0, 1.0)
+    ax.set_ylim(1.0, -1.0)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_title('Visualisation of mispredictions')
+    ax.set_xticks(xstats.x_edge)
+    ax.set_yticks(xstats.y_edge)
+    ax.grid(True, zorder=1, linestyle=':')
+    cbar = plt.colorbar(smap, format=matplotlib.ticker.PercentFormatter(xmax=1))
+    cbar.set_label('Misprediction percentage', rotation=270, labelpad=16)
+    plt.show()
 
 
 if __name__ == '__main__':
